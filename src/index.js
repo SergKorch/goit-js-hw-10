b30e9fb7d0fa2ff79eeae604cfa3229d8e2ff0d7
Serhii Korchemkin <skorchemkin1987@gmail.com>
2022-02-20 23:05:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
